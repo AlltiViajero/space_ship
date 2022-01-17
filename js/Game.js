cc002101a671a8844c26167233b04f4c78124eec
Wojciech Siudy <wojciechsiudy96@gmail.com>
2022-01-17 15:15:21 +0100

    add condition collisions enemy with missile

--- js/Game.js
+++ js/Game.js
@@ -12,7 +12,7 @@ class Game{
     #enemiesInterval = null;
     init(){
         this.#ship.init();
-        this.#newGame();
+        //this.#newGame();
     }
     #newGame(){
         this.#enemiesInterval = 30;
@@ -23,8 +23,8 @@ class Game{
     #randomNewEnemy(){
       const randomNumber = Math.floor(Math.random() * 5) +1;
       randomNumber % 5 ? 
-        this.#createNewEnemy(this.#HTML_ELEMENTS.contener, this.#enemiesInterval, 'enemy') : 
-        this.#createNewEnemy(this.#HTML_ELEMENTS.contener, this.#enemiesInterval * 2, 'enemy--big', 3)
+        this.#createNewEnemy(this.#HTML_ELEMENTS.contener, this.#enemiesInterval, 'enemy', 'explosion') : 
+        this.#createNewEnemy(this.#HTML_ELEMENTS.contener, this.#enemiesInterval * 2, 'enemy--big', 'explosion--big', 3)
     }
 //metoda tworząca obiekty wrogów
     #createNewEnemy(...params){
@@ -37,26 +37,40 @@ class Game{
         this.#enemies.forEach((enemy, enemyIndex, enemiesArray) => {
             const enemyPosition = {
                 top: enemy.element.offsetTop,
-                right: enemy.element.Left + enemy.element.offsetWidth,
+                right: enemy.element.offsetLeft + enemy.element.offsetWidth,
                 bottom: enemy.element.offsetTop + enemy.element.offsetHeight,
                 left: enemy.element.offsetLeft,
             }
             if (enemyPosition.top > window.innerHeight) {
-                enemy.remove();
-                enemiesArray.splice(enemyIndex, 1)
+                enemy.explode();
+                enemiesArray.splice(enemyIndex, 1);
             };
-        });
-        this.#ship.missiles.forEach((missile, missileIndex, missileArray) => {
-            const missilePosition = {
-                top: missile.element.offsetTop,
-                right: missile.element.Left + missile.element.offsetWidth,
-                bottom: missile.element.offsetTop + missile.element.offsetHeight,
-                left: missile.element.offsetLeft,
-            }
-            if (missilePosition.bottom < 0) {
-                missile.remove();
-                missileArray.splice(missileIndex, 1)
-            };
+            this.#ship.missiles.forEach((missile, missileIndex, missileArray) => {
+                const missilePosition = {
+                    top: missile.element.offsetTop,
+                    right: enemy.element.offsetLeft + enemy.element.offsetWidth,
+                    bottom: missile.element.offsetTop + missile.element.offsetHeight,
+                    left: missile.element.offsetLeft,
+                }
+                if (
+                    missilePosition.bottom >= enemyPosition.top && 
+                    missilePosition.top <= enemyPosition.bottom &&
+                    missilePosition.right >= enemyPosition.left && 
+                    missilePosition.left <= enemyPosition.right
+                    ){
+                        enemy.hit();
+                        if (!enemy.lives){
+                            enemiesArray.splice(enemyIndex, 1);
+                        }
+                        missile.remove();
+                        missileArray.splice(missileIndex, 1);
+                    };
+                    
+                if (missilePosition.bottom < 0) {
+                    missile.remove();
+                    missileArray.splice(missileIndex, 1);
+                };
+            });
         });
     };
 }
